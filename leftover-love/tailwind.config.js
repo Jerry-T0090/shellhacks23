a273525ac86b819d18f40b171e64989906617002
zaina-qasim <zaina.qasim@blueinnovationsgroup.com>
2023-09-16 12:22:22 -0400

    Added colors

--- leftover-love/tailwind.config.js
+++ leftover-love/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./index.{html,scss}', './src/**/*.{tsx,scss}'],
   theme: {
+    colors: {
+      gunmetal: "#223843ff",
+      "paynes-gray": "#56676fff",
+      "cadet-gray": "#89959bff",
+      "antiflash-white": "#eff1f3ff",
+      timberwolf: "#dbd3d8ff",
+      "desert-sand": "#d8b4a0ff",
+      "atomic-tangerine": "#d89781ff",
+      "burnt-sienna": "#d77a61ff", 
+    },
     extend: {},
   },
   plugins: [],
